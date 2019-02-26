930c2a759f1c97a6798aace26ec45b0d124c59ad
Richard Sherman <rss.pdx@gmail.com>
2019-02-26 14:50:58 -0800

    fix varnames and removed regions

--- mapminder/mapminder_app/views.py
+++ mapminder/mapminder_app/views.py
@@ -7,15 +7,17 @@ from .models import Chart
 
 def index(request):
     if request.method == 'POST':
-        print(request)
-        print(request.POST)
-        chart = Chart.objects.get(id=request.POST['chart_id'])
+        chart_id = int(request.POST['chart_id'])
+        chart = Chart.objects.get(id=chart_id)
         file = chart.filename
         chart_html = file.open(mode='r').read()
     else:
+        chart_id = ''
         chart_html = ''
     charts = Chart.objects.all()
-    context = {'charts': charts, 'chart_html': chart_html}
+    # if chart_id == '':
+        # chart_id = charts[0].id
+    context = {'charts': charts, 'chart_html': chart_html, "chart_id": chart_id}
     return render(request, 'mapminder_app/index.html', context)
 
 def explanation(request):
