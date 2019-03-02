658cde6447616a4ee5c102c445d3afc321dd5bce
Richard Sherman <rss.pdx@gmail.com>
2019-03-02 09:34:11 -0800

    fixed ddl on explanation page

--- mapminder/mapminder_app/views.py
+++ mapminder/mapminder_app/views.py
@@ -14,21 +14,19 @@ def index(request):
     else:
         chart_id = ''
         chart_html = ''
-    charts = Chart.objects.all()
-    # if chart_id == '':
-        # chart_id = charts[0].id
+    charts = Chart.objects.order_by('shortname')
     context = {'charts': charts, 'chart_html': chart_html, "chart_id": chart_id}
     return render(request, 'mapminder_app/index.html', context)
 
 def explanation(request):
     if request.method == 'POST':
-        print(request)
-        print(request.POST)
-        chart = Chart.objects.get(id=request.POST['chart_id'])
+        chart_id = int(request.POST['chart_id'])
+        chart = Chart.objects.get(id=chart_id)
         file = chart.filename
         chart_html = file.open(mode='r').read()
     else:
+        chart_id = ''
         chart_html = ''
-    charts = Chart.objects.all()
-    context = {'charts': charts, 'chart_html': chart_html}
-    return render(request, 'mapminder_app/regression_explanation.html', context)
+    charts = Chart.objects.order_by('shortname')
+    context = {'charts': charts, 'chart_html': chart_html, "chart_id": chart_id}
+    return render(request, 'mapminder_app/index.html', context)
